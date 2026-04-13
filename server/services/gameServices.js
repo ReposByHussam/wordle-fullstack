f3c287dbbd60c602def8b16154fe3c9fec40be73
Hussam Hilow <developedbyhussam@gmail.com>
2026-04-13 17:47:45 +0200

    lade till gameServices med ett enkelt testord

---
+++ server/services/gameServices.js
@@ -0,0 +1,50 @@
+//tillfällig lagring i minnet för pågående spel.
+//Nyckeln blir gameId och värdet blir själva spelobjektet
+const games = new Map();
+
+//funktion för att skapa ett nytt spel med unikt id
+function createGameId() {
+  return Math.random().toString(36).slice(2, 10); //genererar en slumpmässig sträng som används som gameId);
+}
+
+//En funktion som skapar ett nytt spel och sparar det i minnet
+function createGame(settings) {
+    const {wordLength, allowDuplicateLetters} = settings;
+
+    //tillfälligt testord
+    const secretWord = "apple";
+
+    const gameId = createGameId();
+
+    const game = {
+        gameId,
+        secretWord,
+        settings: {
+            wordLength,
+            allowDuplicateLetters,
+        },
+        startedAt: Date.now(),
+        guesses: [],
+        isFinished: false,
+    };
+    //sparar spelet i minnet på servern
+    games.set(gameId, game);
+
+    //skickar tillbaka bara den data frontend behöver
+    return {
+        gameId,
+        wordLength,
+        allowDuplicateLetters,
+        status: "platying",
+    };
+}
+//hämtar ett spel från minnet med hjälp av gameId
+function getGameById(gameId) {
+    return games.get(gameId);
+}
+
+//exporterar funktionerna så att de kan användas i controllers
+module.exports = {
+    createGame,
+    getGameById,
+};
